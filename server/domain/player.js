5ba72280671f3b2238611af8e5eb947223c20592
Phillip Cannon <phillip.alan.cannon@gmail.com>
2021-07-02 18:17:09 -0500

    added players and gameState

---
+++ server/domain/player.js
@@ -0,0 +1,9 @@
+function player(id) {
+  return {
+    id: id,
+    x: 0,
+    y: 0,
+  };
+}
+
+module.exports = { player }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
+let { gameState, addPlayer } = require('./domain/gameState');
+
 const server = require('http').createServer();
 const io = require('socket.io')(server);
+
+
+
 io.on('connection', client => {
+  gameState = addPlayer(gameState, client.id)
+  console.log(gameState)
   client.on('event', data => { /* … */ });
   client.on('disconnect', () => { /* … */ });
 });
