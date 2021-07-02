d9e5ffc7ff3e197fe2f3fc6f1f7a69bb9a86eefa
Skyler Fredrickson <skylerfredric19@gmail.com>
2021-07-02 18:34:23 -0500

    added wizard and movement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import Wizard from './components/wizard';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [x, setX] = useState(100);
+  const [y, setY] = useState(100);
+  const [currentSpeed, setCurrentSpeed] = useState(40);
+  const [keyState, setKeyState] = useState({});
+
+  useEffect(() => {
+    document.addEventListener("keydown",(e) => {  // simple but powerful
+      let newKeyState = {...keyState};
+      newKeyState[e.code] = e.type === "keydown";
+      setKeyState(newKeyState);
+    });
+  }, []);
+  
+  useEffect(() => {
+    const { KeyW, KeyA, KeyS, KeyD} = keyState;
+    //UP
+    if (KeyW) {
+      setY(y - currentSpeed);
+    }
+
+    //DOWN
+    if (KeyS) {
+      setY(y + currentSpeed);
+    }
+
+    //LEFT
+    if (KeyA) {
+      setX(x - currentSpeed);
+    }
+    
+    //RIGHT
+    if (KeyD) {
+      setX(x + currentSpeed)
+    } 
+  }, [keyState]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ position: 'absolute', left: x, top: y}}>
+        <Wizard x={x} y={y} />
+      </div>
     </div>
   );
 }
---
+++ src/components/wizard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Wizard = () => {
+    const style = { width: '100px', height: '100px', borderRadius: '50px', backgroundColor: 'green'};
+    return (<div style={style} />)
+}
+
+export default Wizard;
