f9ef6a2463165dbd98311cffb3a5e1f44577a0f5
Skyler Fredrickson <skylerfredric19@gmail.com>
2021-07-02 19:26:05 -0500

    moved state to wizard and added shot

--- src/App.js
+++ src/App.js
@@ -1,48 +1,9 @@
 import Wizard from './components/wizard';
-import { useState, useEffect } from 'react';
 
 function App() {
-  const [x, setX] = useState(100);
-  const [y, setY] = useState(100);
-  const [currentSpeed, setCurrentSpeed] = useState(40);
-  const [keyState, setKeyState] = useState({});
-
-  useEffect(() => {
-    document.addEventListener("keydown",(e) => {  // simple but powerful
-      let newKeyState = {...keyState};
-      newKeyState[e.code] = e.type === "keydown";
-      setKeyState(newKeyState);
-    });
-  }, []);
-  
-  useEffect(() => {
-    const { KeyW, KeyA, KeyS, KeyD} = keyState;
-    //UP
-    if (KeyW) {
-      setY(y - currentSpeed);
-    }
-
-    //DOWN
-    if (KeyS) {
-      setY(y + currentSpeed);
-    }
-
-    //LEFT
-    if (KeyA) {
-      setX(x - currentSpeed);
-    }
-    
-    //RIGHT
-    if (KeyD) {
-      setX(x + currentSpeed)
-    } 
-  }, [keyState]);
-
   return (
     <div className="App">
-      <div style={{ position: 'absolute', left: x, top: y}}>
-        <Wizard x={x} y={y} />
-      </div>
+      <Wizard />
     </div>
   );
 }
--- src/components/wizard.jsx
+++ src/components/wizard.jsx
@@ -1,8 +1,59 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import Shot from './shot';
 
 const Wizard = () => {
-    const style = { width: '100px', height: '100px', borderRadius: '50px', backgroundColor: 'green'};
-    return (<div style={style} />)
+  const [x, setX] = useState(100);
+  const [y, setY] = useState(100);
+  const [shots, setShots] = useState([]);
+  const [currentDirection, setCurrentDirection] = useState('right');
+  const [currentSpeed, setCurrentSpeed] = useState(40);
+  const [keyState, setKeyState] = useState({});
+
+  useEffect(() => {
+    document.addEventListener("keydown",(e) => {  // simple but powerful
+      let newKeyState = {};
+      newKeyState[e.code] = e.type === "keydown";
+      setKeyState(newKeyState);
+    });
+  }, []);
+  
+  useEffect(() => {
+    const { KeyW, KeyA, KeyS, KeyD, Space } = keyState;
+    console.log(keyState);
+    //UP
+    if (KeyW) {
+      setCurrentDirection('up');
+      setY(y - currentSpeed);
+    }
+
+    //DOWN
+    if (KeyS) {
+      setCurrentDirection('down');
+      setY(y + currentSpeed);
+    }
+
+    //LEFT
+    if (KeyA) {
+      setCurrentDirection('left');
+      setX(x - currentSpeed);
+    }
+    
+    //RIGHT
+    if (KeyD) {
+      setCurrentDirection('right');
+      setX(x + currentSpeed)
+    } 
+
+    if (Space) {
+        let newShots = [...shots];
+        newShots.push(<Shot direction={currentDirection} initX={x + 10} initY={y + 10} />)
+        setShots(newShots);
+    }
+
+  }, [keyState]);
+    const style = { position: 'absolute', left: x, top: y, width: '100px', height: '100px', borderRadius: '50px', backgroundColor: 'green'};
+    return (<div><div id="wizard" style={style} />{shots}</div>)
 }
 
 export default Wizard;
