36c940abe8676f1402b779e76220b6cafab9d842
Skyler Fredrickson <skylerfredric19@gmail.com>
2021-07-02 19:32:00 -0500

    added socketio client back

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import Wizard from './components/wizard';
+import { useEffect} from 'react';
+import { io } from "socket.io-client";
+
+let socket = io("http://localhost:3030");
 
 function App() {
+    useEffect(() => {
+      
+    }, []);
   return (
     <div className="App">
       <Wizard />
